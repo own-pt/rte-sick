58764c5f260f3ee344547e570199860ab8090b48
Fabricio Rosario <fchalub@br.ibm.com>
2017-01-10 21:32:39 -0200

    typo

--- conll/run-all.sh
+++ conll/run-all.sh
@@ -2,4 +2,4 @@ python3 process-freeling-unique.py
 cat sentences.tokens | ../parse.sh > sentences.conll
 python3 combine-unique.py
 rm sentences.conll
-cat *.conll > sentences.conll
+cat *.conll > sentences.conllu
