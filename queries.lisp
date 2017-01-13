c268f34062bf65293ddc7168ef71b25a6816c4ab
Fabricio Rosario <fchalub@br.ibm.com>
2017-01-13 09:21:26 -0200

    add query code

---
+++ queries.lisp
@@ -0,0 +1,22 @@
+(ql:quickload :cl-conllu)
+
+(defpackage :sick
+  (:use :cl :cl-conllu))
+
+(in-package :sick)
+
+(defun neg? (token)
+  (string= "neg" (token-deprel token)))
+
+(defun find-token (id tokens)
+  (car (remove-if-not (lambda (x) (= id (token-id x))) tokens)))
+
+(defun neg-distribution (sentences)
+  (dolist (s sentences)
+    (let* ((tokens (sentence-tokens s))
+           (negs (remove-if-not #'neg? tokens)))
+      (let ((heads (mapcar (lambda (x) (find-token (token-head x) tokens)) negs)))
+        (dolist (h heads)
+          (format t "~a~%" (token-upostag h)))))))
+
+(neg-distribution (read-conllu #p "conll/sentences.conllu"))
