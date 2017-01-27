f5aaebc8e10b74d438e322cc314e3bb12dd78726
Fabricio Rosario <fchalub@br.ibm.com>
2017-01-27 13:32:15 -0200

    removed conll/

--- conll/run-all.sh
+++
@@ -1,5 +0,0 @@
-python3 process-freeling-unique.py
-cat sentences.tokens | ../parsey.sh > sentences-parsey.conllu
-python3 combine-unique.py
-rm sentences-parsey.conllu
-cat *.conll > sentences-parsey.conllu
