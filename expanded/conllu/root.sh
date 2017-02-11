86426032296e70f384a9aa45b722ab9d4025f865
Fabricio Rosario <f@cp300.org>
2017-02-11 17:17:05 -0200

    adding stats for all 3,4,5 length sentences

--- expanded/conllu/root.sh
+++ expanded/conllu/root.sh
@@ -1,2 +1,2 @@
-cat $1 | grep -P "\tROOT\t" | awk -F '\t' '{print $4}' | sort | uniq -c | sort -rn > $1.root.txt
+cat $1 | grep $'\tROOT\t' | awk -F '\t' '{print $4}' | sort | uniq -c | sort -rn > $1.root.txt
 
